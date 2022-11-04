d192730d2f4b216770c8bfdd47209cb00edda809
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-11-04 17:51:27 +0200

    changed event module branch

--- log.js
+++ log.js
@@ -1,23 +1,18 @@
 // створюємо клас
 const EventEmitter = require('events');
-// замість класичного наслідування можна скористатися моделем util. Спочатку імпортуємо його в файл
-const util = require('util');
 // створюємо екземпляр класу - колистворили Logger цей емітер вже не потрібен
 //const emitter = new EventEmitter();
 
 // будемо наслідувати від EventEmitter. Це буде клас наслідник, котрий наслідує всі методи батька, та розширяє своїми кастомними методами
 // після додавання модулю util можна видалити extends
 // class Logger extends EventEmitter {
-class Logger {
+class Logger extends EventEmitter {
     log = (msg) => {
         console.log(msg);
         //звертаємось до його унаслідуваного методу emit
         this.emit('some_event', {id: 1, text: 'Event test text!' });
     }
 }
-
-// для використовуючи метод inherits ми розширяємо сам EventEmitter
-util.inherits(Logger, EventEmitter);
 
 // функція логування
 
