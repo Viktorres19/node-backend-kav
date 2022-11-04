9ea668bb2853e1202b3ec0a1a424f1841eba546d
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-11-04 09:09:21 +0200

    04 file system

--- modules.js
+++
@@ -1,9 +0,0 @@
-// os допомагає отримати різноманітні відомості про операційну систему
-const os = require('os');
-const { userName: user, sayHi } = require('./test');
-
-const name = 'Tommy';
-
-console.log(os.platform(), os.release());
-
-console.log(sayHi(name));
